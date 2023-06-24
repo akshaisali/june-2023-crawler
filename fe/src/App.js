575edd27504e07da5cd36ac7780a3111f2df582a
Akshaisali <akshaisali007@gmail.com>
2023-06-24 11:49:47 +0530

    Error module build failed

--- fe/src/App.js
+++ fe/src/App.js
@@ -6,14 +6,28 @@ import axios, * as others from 'axios';
 
 function App() {
     const [artists, setArtists] = useState([]);
+    const [tracks, setTracks] = useState([])
+    
 
     useEffect(() => {
         axios.get("http://127.0.0.01:8000//api/v1/artist")
             .then((resp) => {
                 setArtists(resp.data.artists);
+                setTracks([])
+                
             });
         },[]);
+        
+        function onClickHandlerTracks(e) {
+            e.preventDefault();
+            const artistId = e.currentTarget.getAttribute('artist_id');
+            axios.get(`http://127.0.0.1:8000/api/v1/artist/${artistId}`)
+                .then((resp) => {
+                    setTracks(resp.data.tracks);
     
+                });
+        }
+        }
   return (
           <div className="row">
           <div className="col">
@@ -22,6 +36,8 @@ function App() {
                           {artists.map(((artist, idx)=><li key={`artist${artist.id}`}>
                                         <a 
                                         href={`http://127.0.0.01:8000/api/v1/artist/${artist.id}`}
+                                        onClick={onClickHandlerTracks}
+                                        artist_id={artist.id}
 
                                         >{artist.name}
                                         </a>
@@ -30,11 +46,20 @@ function App() {
           </div>
           <div className="col">
           <h2> Tracks </h2>
-          </div>
-          <div className="col">
+          <ul>
+                    {tracks.map(((track, idx) => <li key={`track${track.id}`}>
+                        <a
+                            href={`http://127.0.0.1:8000/api/v1/song/${track.id}`}
+                            onClick={onClickHandlerLyrics}
+                            track_id={track.id}
+                        >{track.name}
+                        </a>
+                    </li>))}
+                </ul>
+            </div>
           <h2> Lyrics </h2>
           </div>
-          </div>
+          
   );
 }
 
